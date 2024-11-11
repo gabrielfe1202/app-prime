50d523964b0c00ff8dbf1d4d66d11e6adcb901b0
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-11-11 15:03:10 -0300

    fix: api data params

--- controllers/kid.controller.ts
+++ controllers/kid.controller.ts
@@ -121,8 +121,7 @@ export class KidController {
         try {
             const response = await api.get('/Cri_imagem', {
                 params: {
-                    id: this.Id,
-                    token: 'f0a625f7-e83b-4e20-8b40-03fb4606eaa8'
+                    id: this.Id
                 }
             });
 
