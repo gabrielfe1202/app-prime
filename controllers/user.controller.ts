56898f654262e44a0180970b920a5b0cb3c9eb12
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-11-11 17:06:01 -0300

    feat: user infos and childs from api

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -13,7 +13,6 @@ type LoginResponse = {
     msg: string
 }
 
-
 export class UserController {
     private token = 'f0a625f7-e83b-4e20-8b40-03fb4606eaa8';
 
@@ -29,16 +28,23 @@ export class UserController {
             usuario: userSchema
         })
 
-        const result = requestSchema.safeParse(this.data);
-
         const user = new User()
 
-        if (result.error) return user;
+        try {
+            const response = await api.get('/Dados');
 
-        user.id = result.data.usuario.Idt_usuario;
-        user.name = result.data.usuario.Nom_usuario;
-        user.email = result.data.usuario.Nom_login;
-        user.imageUser = result.data.usuario.Img_usuario;
+            const result = requestSchema.safeParse(response.data);
+
+            if (result.error) return user;
+
+            user.id = result.data.usuario.Idt_usuario;
+            user.name = result.data.usuario.Nom_usuario;
+            user.email = result.data.usuario.Nom_login;
+            user.imageUser = result.data.usuario.Img_usuario;
+
+        } catch (error) {
+            console.error(error)            
+        }
 
         return user;
     }
@@ -55,7 +61,7 @@ export class UserController {
 
     async login(user: string, password: string): Promise<LoginResponse> {
 
-        try {           
+        try {
             const requestShape = z.object({
                 token: z.string().optional(),
                 errorMsg: z.string().optional()
@@ -65,7 +71,7 @@ export class UserController {
                 "login": user,
                 "senha": password
             });
-            
+
             const result = requestShape.safeParse(response.data)
 
             if (result.error) return {
@@ -73,20 +79,20 @@ export class UserController {
                 msg: "Erro ao efetuar login",
             }
 
-            if(result.data.errorMsg != undefined){
+            if (result.data.errorMsg != undefined) {
                 return {
                     status: "ERROR",
                     msg: result.data.errorMsg,
-                }    
+                }
             }
 
-            if(result.data.token != undefined){
+            if (result.data.token != undefined) {
                 await AsyncStorage.setItem('@Primeapp:usertoken', result.data.token)
                 return {
                     status: "SUCCESS",
                     token: result.data.token
                 }
-            }            
+            }
 
             return {
                 status: "ERROR",
@@ -117,164 +123,28 @@ export class UserController {
             filhos: z.array(childShape)
         })
 
-        const result = requestShape.safeParse(this.dataChild)
+        try{
+            const response = await api.get("/Filhos")
 
-        if (result.error) return []
+            const result = requestShape.safeParse(response.data)
 
-        const childs = result.data.filhos.map(_filho => {
-            const child = new Kid()
+            if (result.error) return []
 
-            child.Idt_Cri_Crianca = _filho.Idt_Cri_Crianca;
-            child.Nome = _filho.Nome;
-            child.imagem = _filho.imagem;
+            const childs = result.data.filhos.map(_filho => {
+                const child = new Kid()
+    
+                child.Idt_Cri_Crianca = _filho.Idt_Cri_Crianca;
+                child.Nome = _filho.Nome;
+                child.imagem = _filho.imagem;
+    
+                return child
+            })
+    
+            return childs
 
-            return child
-        })
-
-        return childs
-
-    }
-
-    private data = {
-        "usuario": {
-            "Idt_usuario": 3,
-            "Idt_cliente": null,
-            "Nom_cliente": null,
-            "Idt_empresa": null,
-            "Nom_empresa": null,
-            "Idt_generico": null,
-            "Nom_usuario": "André Dias",
-            "Nom_user": null,
-            "Nom_login": "andre@o2ew.com.br",
-            "Nom_frase_senha": "",
-            "Nom_login_criador": null,
-            "Idc_usuario_ativo": true,
-            "Nom_usuario_ativo": null,
-            "Sal": "00000000-0000-0000-0000-000000000000",
-            "Idt_Programa": null,
-            "Nom_Programa": null,
-            "nom_telefone": null,
-            "nom_celular": null,
-            "nom_funcao": null,
-            "Tip_equipe": null,
-            "Ult_login": null,
-            "DataFormatada": null,
-            "Idt_tip_equipe": 6,
-            "tip_equipe": null,
-            "Img_usuario": "https://sistema.primetimecd.com.br/imgsist/criancas/1411202200435669iaradonatodias.jpg",
-            "End_endereco": null,
-            "End_numero": null,
-            "End_complemento": null,
-            "End_cep": null,
-            "End_bairro": null,
-            "End_cidade": null,
-            "End_estado": null,
-            "End_comercial_endereco": null,
-            "End_comercial_numero": null,
-            "End_comercial_complemento": null,
-            "End_comercial_cep": null,
-            "End_comercial_bairro": null,
-            "End_comercial_cidade": null,
-            "End_comercial_estado": null,
-            "Cpf_usuario": null,
-            "Rg_usuario": null,
-            "responsavel_financeiro": null,
-            "equipe": null,
-            "filho_ativo": 0
+        }catch (error) {
+            console.log(error)
+            return []
         }
     }
-
-    private dataChild = {
-        "filhos": [
-            {
-                "Idt_Cri_Crianca": 69,
-                "Nome": "IARA DONATO DIAS",
-                "email": null,
-                "data_nasc": null,
-                "data_bercario": null,
-                "data_maternal": null,
-                "endereco": null,
-                "numero": null,
-                "complemento": null,
-                "cep": null,
-                "bairro": null,
-                "cidade": null,
-                "estado": null,
-                "nome_ped": null,
-                "email_ped": null,
-                "telefone_ped": null,
-                "endereco_ped": null,
-                "numero_ped": null,
-                "complemento_ped": null,
-                "cep_ped": null,
-                "bairro_ped": null,
-                "cidade_ped": null,
-                "estado_ped": null,
-                "tipo_sala": null,
-                "sala": null,
-                "nom_pasta": "69-16112023135122",
-                "zip_foto": null,
-                "Idt_site": null,
-                "ativo": null,
-                "ativo_sim": null,
-                "titulos": null,
-                "pais": null,
-                "educadoras": null,
-                "imagens": null,
-                "reuniao": null,
-                "agenda": null,
-                "imagem": "https://o2sistema.blob.core.windows.net/o2sistema/Primetime/69-16112023135122/media/10320151140174.jpg",
-                "data_formata": null,
-                "imagem_crianca": null,
-                "agora": "1032015114017",
-                "contratos": 0,
-                "contratos_m": 0
-            },
-            {
-                "Idt_Cri_Crianca": 295,
-                "Nome": "Criança para teste",
-                "email": null,
-                "data_nasc": null,
-                "data_bercario": null,
-                "data_maternal": null,
-                "endereco": null,
-                "numero": null,
-                "complemento": null,
-                "cep": null,
-                "bairro": null,
-                "cidade": null,
-                "estado": null,
-                "nome_ped": null,
-                "email_ped": null,
-                "telefone_ped": null,
-                "endereco_ped": null,
-                "numero_ped": null,
-                "complemento_ped": null,
-                "cep_ped": null,
-                "bairro_ped": null,
-                "cidade_ped": null,
-                "estado_ped": null,
-                "tipo_sala": null,
-                "sala": null,
-                "nom_pasta": "295-29082024111312",
-                "zip_foto": null,
-                "Idt_site": null,
-                "ativo": null,
-                "ativo_sim": null,
-                "titulos": null,
-                "pais": null,
-                "educadoras": null,
-                "imagens": null,
-                "reuniao": null,
-                "agenda": null,
-                "imagem": "https://o2sistema.blob.core.windows.net/o2sistema/Primetime/295-29082024111312/media/295-290820241526251.jpg",
-                "data_formata": null,
-                "imagem_crianca": null,
-                "agora": "29082024135742",
-                "contratos": 0,
-                "contratos_m": 0
-            }
-        ]
-    }
-
 }
