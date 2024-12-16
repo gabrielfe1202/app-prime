247654e1bf62c752d865d23ef39740ea42bde7ae
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-12-16 11:23:54 -0300

    fix: layout account page

--- app/(Account)/index.tsx
+++ app/(Account)/index.tsx
@@ -1,16 +1,40 @@
 import { BottomTab } from "@/components/BottomTab";
+import { Loading } from "@/components/Loading";
+import { useAppUser } from "@/contexts/UserContext";
+import { User } from "@/entities/user";
 import { FontAwesome } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
 import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { fonts } from "../globalStyle";
 const { width, height } = Dimensions.get("screen")
 
 export default function Account() {
+    const [stateLoad, setStateload] = useState<boolean>(true)
+    const [user, setUser] = useState<User>();
+    const { userController } = useAppUser();
+
+    async function loadUserInfos() {
+        try {
+            const data = await userController.getUserInformations();
+            setUser(data)
+        } finally {
+            setStateload(false)
+        }
+    }
+
+    useEffect(() => {
+        loadUserInfos()
+    }, [])
+
+    if (stateLoad) return <Loading />
+
     return (
         <GestureHandlerRootView>
             <SafeAreaView style={styles.container}>
-                <View style={{justifyContent: "center", alignItems: "center"}}>
-                    <FontAwesome name="user-circle" size={80} color={"#000"} />                
-                    <Text>Nome aqui</Text>
+                <View style={{ justifyContent: "center", alignItems: "center" }}>
+                    <FontAwesome name="user-circle" size={80} color={"#000"} />
+                    <Text style={styles.userName}>{user?.name}</Text>
                 </View>
 
                 <View style={styles.box}>
@@ -19,8 +43,7 @@ export default function Account() {
                     </View>
 
                     <View style={styles.boxData}>
-                        <Text>Email:  teste@teste.com</Text>
-                        <Text>Telefone: 790497004040</Text>
+                        <Text style={styles.boxDataText}>Email: {user?.email}</Text>
                     </View>
 
                     <TouchableOpacity style={styles.boxButton}>
@@ -55,6 +78,12 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     },
+    userName: {
+        fontFamily: fonts.passo,
+        fontSize: 22,
+        paddingTop: 10,
+        paddingBottom: 20
+    },
     box: {
         width: width * .85,
         borderWidth: 1,
@@ -69,24 +98,29 @@ const styles = StyleSheet.create({
         paddingVertical: 5
     },
     boxTileText: {
-        fontSize: 18
+        fontSize: 18,
+        fontFamily: fonts.passo,
     },
     boxData: {
         paddingHorizontal: 30,
-        paddingVertical: 5,
+        paddingVertical: 15,
         gap: 10,
-        paddingBottom: 10
+    },
+    boxDataText: {
+        fontFamily: fonts.passo,
+        fontSize: 16
     },
     boxButton: {
         flexDirection: "row",
         gap: 10,
         paddingHorizontal: 20,
         paddingVertical: 10,
-        borderTopWidth: 1,  
-        borderTopColor: "#a8a7a7",        
+        borderTopWidth: 1,
+        borderTopColor: "#a8a7a7",
         alignItems: "center"
     },
     boxButtonText: {
-        fontSize: 18
+        fontSize: 18,
+        fontFamily: fonts.passo,
     }
 })
